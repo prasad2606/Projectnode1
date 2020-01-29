cbe894714fbdf029c7cfc5385250ce0912d862ec
prasad <prasad.almelkar@in.ibm.com>
2020-01-29 10:20:50 +0530

    Add your commit message

---
+++ routes.js
@@ -0,0 +1,66 @@
+var express =  require("express");
+var router = express.Router();
+var http=require("http");
+
+router.get("/", function(req,res){
+    console.log("Hello I am on the basics1");
+    res.render("about");
+});
+
+router.get("/Categories", function(req,res){
+    console.log("Hello I am on the basics");
+    var options =
+    { "method":"GET",
+      "host":"dbapp-01-cloud-atlas.inmbzp8022.in.dst.ibm.com",
+      "path":"/api/TestDb/AllStyles",
+      "headers": {"Cache-Control": "no-cache",}
+    };
+    var x = http.request(options,function(res1)
+    {       
+        var chunks = [];
+        res1.on("data", function (chunk) {
+            chunks.push(chunk);
+        });
+        res1.on("end", function () {
+            var body = Buffer.concat(chunks);
+            var data= JSON.parse(body);
+            //console.log(data);
+            //res.send(data);
+            //return fnc(data);
+            res.render("category",{ data:data});
+        });
+
+    });
+    x.end();
+   // res.send("index1");
+});
+router.get("/Products", function(req,res){
+    console.log("Hello I am on the basics2");
+    var options =
+    { "method":"GET",
+      "host":"dbapp-01-cloud-atlas.inmbzp8022.in.dst.ibm.com",
+      "path":"/api/TestDb/AllStyles",
+      "headers": {"Cache-Control": "no-cache",}
+    };
+    var x = http.request(options,function(res1)
+    {       
+        var chunks = [];
+        res1.on("data", function (chunk) {
+            chunks.push(chunk);
+        });
+        res1.on("end", function () {
+            var body = Buffer.concat(chunks);
+            var data= JSON.parse(body);
+            //console.log(data);
+            //res.send(data);
+            //return fnc(data);
+            res.render("product",{ data:data});
+        });
+
+    });
+    x.end();
+   // res.send("index1");
+});
+
+
+module.exports = router;
